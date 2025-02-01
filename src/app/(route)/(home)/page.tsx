76ba0c43d2201a056d3207ba2da60de1849ebca9
bsy1141 <bsy1141@naver.com>
2025-02-01 17:16:41 +0900

    feat: Card 컴포넌트 초안(변경예정)

--- src/app/(route)/(home)/page.tsx
+++ src/app/(route)/(home)/page.tsx
@@ -3,12 +3,23 @@ import PrefetchHydration from '@/providers/PrefetchHydration';
 import testApis from '@/query-hooks/useTest/api';
 
 import { Test } from './components/Test';
+import QuestionCard from '@/components/Card/Card';
 
 export default async function Home() {
   return (
     <PrefetchHydration queryKey={['test']} queryFn={testApis.getTest}>
       <Test />
       <Button label="button" size="large" />
+      <QuestionCard
+        title="Q. 커리어를 향상시킬 수 있는 일자리이지만 가까운 사람들과 멀어져야한다면, 이 일자리를 선택하실 건가요?"
+        chipContents="1월 8일 1회차"
+        useCollapse
+      >
+        <div className="body2">
+          가까운 사람들과 물리적으로 멀어지더라도 그 관계가 사라지진 않음. 내 노력에 따라 관계는 달라질 수 있지만 커리어
+          기회는 원할 때 오는게 아님
+        </div>
+      </QuestionCard>
       <h3 className="title2">타이포 적용</h3>
     </PrefetchHydration>
   );
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,60 @@
+import { PropsWithChildren } from 'react';
+import { Accordion } from 'radix-ui';
+import { Card as CardComponent, Text } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+import styles from './Card.module.scss';
+
+interface CardProps {
+  title: string;
+  chipContents: string;
+  useCollapse?: boolean;
+}
+
+const BasicCard = ({ title, chipContents, children }: PropsWithChildren<CardProps>) => (
+  <div className={styles.container}>
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <Text color="gray" size="2">
+          {chipContents}
+        </Text>
+        <h3 className="body1">{title}</h3>
+      </div>
+    </div>
+    {children}
+  </div>
+);
+
+export default function Card({ title, chipContents, useCollapse = false, children }: PropsWithChildren<CardProps>) {
+  if (!useCollapse) {
+    return (
+      <BasicCard title={title} chipContents={chipContents}>
+        {children}
+      </BasicCard>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <Accordion.Root type="single" collapsible>
+        <Accordion.Item value="item-1">
+          <CardComponent className={styles.wrapper}>
+            <Accordion.Header className={styles.header}>
+              <div className={styles.chips}>
+                {/* Chip 컴포넌트로 교체 예정 */}
+                <Text color="gray" size="2">
+                  {chipContents}
+                </Text>
+                <Accordion.Trigger>
+                  {/* 아이콘 교체 시 수정 예정 */}
+                  <div>더보기</div>
+                </Accordion.Trigger>
+              </div>
+              <h3 className="body1">{title}</h3>
+            </Accordion.Header>
+            <Accordion.Content>{children}</Accordion.Content>
+          </CardComponent>
+        </Accordion.Item>
+      </Accordion.Root>
+    </div>
+  );
+}
