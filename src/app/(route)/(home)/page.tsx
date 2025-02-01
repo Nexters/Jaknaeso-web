9e620d289c6af22845988fc5cf3ea14409d5080e
bsy1141 <bsy1141@naver.com>
2025-02-01 22:38:19 +0900

    chore: 메인 페이지 내 prefetch 로직 임시 제거

--- src/app/(route)/(home)/page.tsx
+++ src/app/(route)/(home)/page.tsx
@@ -1,16 +1,10 @@
-import PrefetchHydration from '@/providers/PrefetchHydration';
-import testApis from '@/query-hooks/useTest/api';
-
 import { Button } from '@/components/Button';
 import { Card } from '@/components/Card';
 import { Chip } from '@/components/Chip';
 
-import { Test } from './components/Test';
-
 export default async function Home() {
   return (
-    <PrefetchHydration queryKey={['test']} queryFn={testApis.getTest}>
-      <Test />
+    <div>
       <Button label="button" size="large" />
       <Card
         title="Q. 커리어를 향상시킬 수 있는 일자리이지만 가까운 사람들과 멀어져야한다면, 이 일자리를 선택하실 건가요?"
@@ -25,7 +19,6 @@ export default async function Home() {
       <Chip size="md" color="brand">
         Text
       </Chip>
-      {/* <h3 className="title2">타이포 적용</h3> */}
-    </PrefetchHydration>
+    </div>
   );
 }
