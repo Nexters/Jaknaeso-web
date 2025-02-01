6837cfc04a447a86d09ab0cefe64c452ebab1ab1
bsy1141 <bsy1141@naver.com>
2025-02-01 18:44:03 +0900

    feat: Chip 컴포넌트 구현 완료

--- src/app/(route)/(home)/page.tsx
+++ src/app/(route)/(home)/page.tsx
@@ -1,16 +1,17 @@
-import { Button } from '@/components/button';
+import { Button } from '@/components/Button';
 import PrefetchHydration from '@/providers/PrefetchHydration';
 import testApis from '@/query-hooks/useTest/api';
 
 import { Test } from './components/Test';
-import QuestionCard from '@/components/Card/Card';
+import { Card } from '@/components/Card';
+import { Chip } from '@/components/Chip';
 
 export default async function Home() {
   return (
     <PrefetchHydration queryKey={['test']} queryFn={testApis.getTest}>
       <Test />
       <Button label="button" size="large" />
-      <QuestionCard
+      <Card
         title="Q. 커리어를 향상시킬 수 있는 일자리이지만 가까운 사람들과 멀어져야한다면, 이 일자리를 선택하실 건가요?"
         chipContents="1월 8일 1회차"
         useCollapse
@@ -19,8 +20,11 @@ export default async function Home() {
           가까운 사람들과 물리적으로 멀어지더라도 그 관계가 사라지진 않음. 내 노력에 따라 관계는 달라질 수 있지만 커리어
           기회는 원할 때 오는게 아님
         </div>
-      </QuestionCard>
-      <h3 className="title2">타이포 적용</h3>
+      </Card>
+      <Chip size="md" color="brand">
+        Text
+      </Chip>
+      {/* <h3 className="title2">타이포 적용</h3> */}
     </PrefetchHydration>
   );
 }
