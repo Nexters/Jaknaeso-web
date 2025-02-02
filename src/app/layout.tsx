2258b31496c45abbc30a270450cdb66f56338033
bsy1141 <bsy1141@naver.com>
2025-02-02 14:38:15 +0900

    chore: eslint warning 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
 import ReactQueryProviders from '../providers/ReactQueryProvider';
+
 import './globals.scss';
 
 const geistSans = Geist({
