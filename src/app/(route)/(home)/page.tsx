b2aa4172c18beb9268b096f3d0c60c6a4690d65b
suminhan123 <suminh324@gmail.com>
2025-02-03 21:05:41 +0900

    feat: LockBtn 컴포넌트 추가

--- src/app/(route)/(home)/page.tsx
+++ src/app/(route)/(home)/page.tsx
@@ -1,3 +1,13 @@
+import { LockBtn } from '@/components/LockBtn';
+
 export default function Home() {
-  return <div>홈 페이지</div>;
+  return (
+    <div>
+      홈 페이지
+      <LockBtn label="1회차" />
+      <LockBtn label="1회차" disabled />
+      <LockBtn label="1회차" variant="completed" />
+      <LockBtn label="1회차" variant="completedToday" />
+    </div>
+  );
 }
