a6bcb1f4dc03ac006823bb8422f39308f238e991
suminhan123 <suminh324@gmail.com>
2025-02-05 00:43:28 +0900

    feat: 게임 페이지로 라우팅 추가

--- src/app/(route)/(home)/page.tsx
+++ src/app/(route)/(home)/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
 
 import { LineIcon } from '@/assets/icons';
 import { Button } from '@/components/Button';
 import { Capsule } from '@/components/Capsule';
 import { Chip } from '@/components/Chip';
 import { LockBtn } from '@/components/LockBtn';
+import { ROUTES } from '@/constants';
 
 import styles from './page.module.scss';
 const Drawer = dynamic(() => import('./components/Drawer/Drawer'), { ssr: false });
@@ -14,6 +16,7 @@ const DrawerContent = dynamic(() => import('./components/Drawer/Content'), { ssr
 const DrawerFooter = dynamic(() => import('./components/Drawer/Footer'), { ssr: false });
 
 export default function Home() {
+  const routes = useRouter();
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -32,23 +35,53 @@ export default function Home() {
       <Drawer>
         <DrawerContent>
           <div className={styles.grid}>
-            <LockBtn label="1회차" className={styles.lock} />
-            <LockBtn label="1회차" className={styles.lock} disabled />
-            <LockBtn label="1회차" className={styles.lock} variant="completed" />
-            <LockBtn label="1회차" className={styles.lock} variant="completedToday" />
-            <LockBtn label="1회차" className={styles.lock} />
+            <LockBtn label="1회차" className={styles.lock} onClick={() => routes.push(ROUTES.game)} />
+            <LockBtn label="1회차" className={styles.lock} disabled onClick={() => routes.push(ROUTES.game)} />
+            <LockBtn
+              label="1회차"
+              className={styles.lock}
+              variant="completed"
+              onClick={() => routes.push(ROUTES.game)}
+            />
+            <LockBtn
+              label="1회차"
+              className={styles.lock}
+              variant="completedToday"
+              onClick={() => routes.push(ROUTES.game)}
+            />
+            <LockBtn label="1회차" className={styles.lock} onClick={() => routes.push(ROUTES.game)} />
 
-            <LockBtn label="1회차" className={styles.lock} />
-            <LockBtn label="1회차" className={styles.lock} disabled />
-            <LockBtn label="1회차" className={styles.lock} variant="completed" />
-            <LockBtn label="1회차" className={styles.lock} variant="completedToday" />
-            <LockBtn label="1회차" className={styles.lock} />
+            <LockBtn label="1회차" className={styles.lock} onClick={() => routes.push(ROUTES.game)} />
+            <LockBtn label="1회차" className={styles.lock} onClick={() => routes.push(ROUTES.game)} disabled />
+            <LockBtn
+              label="1회차"
+              className={styles.lock}
+              onClick={() => routes.push(ROUTES.game)}
+              variant="completed"
+            />
+            <LockBtn
+              label="1회차"
+              className={styles.lock}
+              onClick={() => routes.push(ROUTES.game)}
+              variant="completedToday"
+            />
+            <LockBtn label="1회차" className={styles.lock} onClick={() => routes.push(ROUTES.game)} />
 
-            <LockBtn label="1회차" className={styles.lock} />
-            <LockBtn label="1회차" className={styles.lock} disabled />
-            <LockBtn label="1회차" className={styles.lock} variant="completed" />
-            <LockBtn label="1회차" className={styles.lock} variant="completedToday" />
-            <LockBtn label="1회차" className={styles.lock} />
+            <LockBtn label="1회차" className={styles.lock} onClick={() => routes.push(ROUTES.game)} />
+            <LockBtn label="1회차" className={styles.lock} onClick={() => routes.push(ROUTES.game)} disabled />
+            <LockBtn
+              label="1회차"
+              className={styles.lock}
+              onClick={() => routes.push(ROUTES.game)}
+              variant="completed"
+            />
+            <LockBtn
+              label="1회차"
+              className={styles.lock}
+              onClick={() => routes.push(ROUTES.game)}
+              variant="completedToday"
+            />
+            <LockBtn label="1회차" className={styles.lock} onClick={() => routes.push(ROUTES.game)} />
           </div>
         </DrawerContent>
         <DrawerFooter>
