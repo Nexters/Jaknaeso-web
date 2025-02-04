60a8c5389970b861947bbb281b1b4dfe0a5efa3b
pythonstrup <pythonstrup@gmail.com>
2025-02-04 22:22:04 +0900

    chore: topBar 오류 수정

--- src/app/(route)/report/layout.tsx
+++ src/app/(route)/report/layout.tsx
@@ -1,7 +1,6 @@
 import type { PropsWithChildren } from 'react';
 
 import { FooterLayout } from '@/components/Layout/Footer';
-import { TopBar } from '@/components/Layout/TopBar';
 
 export default function ReportLayout({ children }: PropsWithChildren) {
   return (
