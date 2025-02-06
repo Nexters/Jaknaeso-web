5581683972c11582ed8fd4ce76eaaf5fb89fa571
bsy1141 <bsy1141@naver.com>
2025-02-07 00:29:54 +0900

    feat: 오지선다 페이지 퍼블리싱 및 로컬 스토리지 값에 따른 분기 처리

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -8,6 +8,7 @@ export const ROUTES = {
   reportQuestions: '/report/questions',
   mypage: '/mypage',
   game: '/game',
+  gameComplete: '/game/complete',
 } as const;
 
 // TODO : 추후에 슬라이더/이중택일 쿼리 적용
