c193154d54aa30e189e039df8d1b2b229cd086af
suminhan123 <suminh324@gmail.com>
2025-02-09 21:21:24 +0900

    feat: 로그인 후 user get api 호출 및 상태 적용

--- src/query-hooks/useAuth/index.ts
+++ src/query-hooks/useAuth/index.ts
@@ -3,16 +3,26 @@ import { useMutation } from '@tanstack/react-query';
 
 import { ROUTES } from '@/constants';
 import { setTokens } from '@/libs/cookie/manageCookie.client';
+import { useMemberStore } from '@/stores';
+
+import memberApis from '../useMember/api.client';
 
 import authApis from './api';
 
 export const useAuthMutation = () => {
   const router = useRouter();
+  const { setMember } = useMemberStore();
   const postKakaoAuth = useMutation({
     mutationFn: authApis.postKakao,
     onSuccess: (res) => {
       setTokens(res.data.data.accessToken, res.data.data.refreshToken);
       router.push(ROUTES.home);
+      memberApis.get(res.data.data.memberId).then((member) => {
+        setMember({
+          ...member,
+          memberId: res.data.data.memberId,
+        });
+      });
     },
   });
   const postAppleAuth = useMutation({
@@ -20,6 +30,12 @@ export const useAuthMutation = () => {
     onSuccess: (res) => {
       setTokens(res.data.data.accessToken, res.data.data.refreshToken);
       router.push(ROUTES.home);
+      memberApis.get(res.data.data.memberId).then((member) => {
+        setMember({
+          ...member,
+          memberId: res.data.data.memberId,
+        });
+      });
     },
   });
 
