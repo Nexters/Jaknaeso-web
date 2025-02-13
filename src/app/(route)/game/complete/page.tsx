25c2dff2246ec13adb52f4232c044bd97c311aa9
bsy1141 <bsy1141@naver.com>
2025-02-13 23:41:21 +0900

    chore: 주석 삭제

--- src/app/(route)/game/complete/page.tsx
+++ src/app/(route)/game/complete/page.tsx
@@ -11,6 +11,7 @@ export default function GameComplete() {
   const onClick = () => {
     router.push('/');
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -21,23 +22,7 @@ export default function GameComplete() {
         </h3>
         <p className={styles.subTitle}>내일도 답변을 완료해 내 캐릭터를 완성해 보세요</p>
       </div>
-      <div className={styles.content}>
-        {/* 로티 이미지 들어갈 부분, 임시 조치 */}
-        {/* <div
-          style={{
-            width: '220px',
-            height: '220px',
-            borderRadius: '1.625rem',
-            background: '#F8F8FA',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: '#6A6A6A',
-          }}
-        >
-          로티 애니메이션
-        </div> */}
-      </div>
+      <div className={styles.content}></div>
       <div className={styles.footer}>
         <Button color="primary" onClick={onClick}>
           완료
