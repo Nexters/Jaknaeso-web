283df4f3ab78f00a1722c0b3f388c616fee027ec
suminhan123 <suminh324@gmail.com>
2025-02-15 11:50:24 +0900

    feat: 홈 캐릭터 스타일 적용

--- src/app/(route)/(home)/components/Page/Title.tsx
+++ src/app/(route)/(home)/components/Page/Title.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { FreeImg } from '@/assets/illustration';
 import { Chip } from '@/components/Chip';
 
 import styles from './Page.module.scss';
@@ -8,10 +9,19 @@ export default function HomeTitle() {
   return (
     <>
       <div className={styles.content}>
-        <Chip size="md" color="brand">
+        <Chip size="md" color="brand" className={styles.chip}>
           첫번째 캐릭터
         </Chip>
         <h1 className="title01">절묘한 균형 추구자 유형</h1>
+      </div>
+      <div className={styles.character}>
+        <FreeImg width={340} height={340} />
+        {/* <AchievementImg width={340} height={340} /> */}
+        {/* <AdventureImg width={340} height={340} /> */}
+        {/* <GeneralityImg width={340} height={340} /> */}
+        {/* <PhilanthropyImg width={340} height={340} /> */}
+        {/* <SafetyImg width={340} height={340} /> */}
+        {/* <StabilityImg width={340} height={340} /> */}
       </div>
     </>
   );
