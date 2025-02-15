5114cc95d24f875d1a47067cf41fceeb0c7c8e9c
suminhan123 <suminh324@gmail.com>
2025-02-15 21:05:49 +0900

    chore: enum 및 캐릭터 상수 수정

--- src/constants/enums/character.ts
+++ src/constants/enums/character.ts
@@ -2,12 +2,12 @@
  * @description 캐릭터 타입 enum
  */
 export enum CHARACTER_TYPE {
-  achievement = 'SUCCESS', // 성공(성취)
-  adventure = 'ADVENTURE', // 모험
-  free = 'SELF_DIRECTION', // 자기 주도(자율)
-  generality = 'UNIVERSALISM', // 보편
-  philanthropy = 'BENEVOLENCE', // 박애
-  safety = 'SECURITY', // 안전
-  stability = 'STABILITY', // 안정
+  SUCCESS = 'SUCCESS',
+  ADVENTURE = 'ADVENTURE',
+  SELF_DIRECTION = 'SELF_DIRECTION',
+  UNIVERSALISM = 'UNIVERSALISM',
+  BENEVOLENCE = 'BENEVOLENCE',
+  SECURITY = 'SECURITY',
+  STABILITY = 'STABILITY',
 }
 export type CharacterKeys = keyof typeof CHARACTER_TYPE;
