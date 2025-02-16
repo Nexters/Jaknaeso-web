86d274034cbf7903a56e707cef8562a45f7eee2e
suminhan123 <suminh324@gmail.com>
2025-02-16 23:16:12 +0900

    feat: 멤버 스토어 이름 적용

--- src/app/(route)/report/components/AnalysisTab/AnalysisContent/ChartContent/ChartContent.tsx
+++ src/app/(route)/report/components/AnalysisTab/AnalysisContent/ChartContent/ChartContent.tsx
@@ -1,12 +1,14 @@
 import { CHARACTERS } from '@/constants';
 import { useGetCharacterAnalysis } from '@/query-hooks/useCharacter';
 import type { CharacterReports } from '@/query-hooks/useCharacter/types';
+import { useMemberStore } from '@/stores';
 
 import styles from './ChartContent.module.scss';
 import RadarChart from './RadarChart';
 
 export default function ChartContent({ characterId }: { characterId: string }) {
   const { data } = useGetCharacterAnalysis(characterId);
+  const { member } = useMemberStore();
 
   const getChartData = (reports: CharacterReports[]) => {
     const labels = reports.map((report) => CHARACTERS[report.keyword].label);
@@ -22,7 +24,7 @@ export default function ChartContent({ characterId }: { characterId: string }) {
     <div className={styles.container}>
       <div className={styles.title}>
         <h2 className="title3">가치관 선택 비율</h2>
-        <h5 className="subTitle4">userName님은 성장과 평화을 가장 중요시 여기고 있어요.</h5>
+        <h5 className="subTitle4">{`${member.name}님은 성장과 평화을 가장 중요시 여기고 있어요.`}</h5>
       </div>
 
       {data && (
