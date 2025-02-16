31f8c5fdc428170b46114229839f6dd6e3cb17d0
suminhan123 <suminh324@gmail.com>
2025-02-16 23:50:56 +0900

    feat: chart max label 적용

--- src/app/(route)/report/components/AnalysisTab/AnalysisContent/ChartContent/ChartContent.tsx
+++ src/app/(route)/report/components/AnalysisTab/AnalysisContent/ChartContent/ChartContent.tsx
@@ -18,13 +18,17 @@ export default function ChartContent({ characterId }: { characterId: string }) {
       .sort((a, b) => b.value - a.value)
       .slice(0, 3)
       .map((item) => item.index);
-    return { labels, percentages, maxIdx };
+    const maxLabels = maxIdx.map((index) => labels[index]);
+
+    return { labels, percentages, maxIdx, maxLabels };
   };
+  const maxLabels = data ? getChartData(data.valueReports).maxLabels : ['', ''];
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <h2 className="title3">가치관 선택 비율</h2>
-        <h5 className="subTitle4">{`${member.name}님은 성장과 평화을 가장 중요시 여기고 있어요.`}</h5>
+        <h5 className="subTitle4">{`${member.name}님은 ${maxLabels[0]}와/과 ${maxLabels[1]}을 가장 중요시 여기고 있어요.`}</h5>
       </div>
 
       {data && (
